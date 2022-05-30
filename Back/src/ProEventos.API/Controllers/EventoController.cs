c59716cf5f55a4003dd4151bce36f1866a7fbeb9
Joao Victor <jvoliveiraplays@gmail.com>
2022-05-30 20:51:49 -0300

    Inicio

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento()
+            {
+                EventoId = 1,
+                Tema = "Angular e .NET",
+                Local = "SP",
+                Lote = "1 lote",
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                QtdPessoas = 250
+            };
+        }
+    }
+}
