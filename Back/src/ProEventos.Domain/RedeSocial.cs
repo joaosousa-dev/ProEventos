54ed74b71f2cad146bf364cefbe2542b79c43b25
Joao Victor <jvoliveiraplays@gmail.com>
2022-06-07 23:07:27 -0300

    implementando ProEventosPersistence

--- Back/src/ProEventos.Domain/RedeSocial.cs
+++ Back/src/ProEventos.Domain/RedeSocial.cs
@@ -9,9 +9,9 @@ namespace ProEventos.Domain
         public string Nome { get; set; }
         public string URL { get; set; }
         public int? EventoId { get; set; }
-        public IEnumerable<Evento> Eventos { get; set; }
+        public Evento Evento { get; set; }
         public int? PalestranteId { get; set; }
-        public IEnumerable<Palestrante> Palestrantes { get; set; }
+        public Palestrante Palestrante { get; set; }
 
     }
 }
